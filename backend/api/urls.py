b6d7333db7632b74c29dccf4039a2055e5240faf
Mitchell <mitch.heidbrink@gmail.com>
2024-08-01 21:23:05 -0400

    finish up backend

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
 from . import views
 
-urlPatterns = [
+urlpatterns = [
     path('activities/', views.ActivityListCreateView.as_view(), name='activity-list-create'),
     path('activities/<int:pk>', views.ActivityDetailView.as_view(), name='activity-detail'),
     path('friends/', views.FriendshipListCreateView.as_view(), name='friendship-list-create'),
-    path('friends/<int:pk', views.FriendshipDetailView.as_view(), name="friendship-detail"),
+    path('friends/<int:pk>', views.FriendshipDetailView.as_view(), name="friendship-detail"),
 ]
