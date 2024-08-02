c51b6b80859d2bcaffe3fd1dbd8e89d08b9beeda
Mitchell <mitch.heidbrink@gmail.com>
2024-08-01 22:46:21 -0400

    add routes in app.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Login from './components/Login';
+import Register from './components/Register';
+import Calendar from './components/Calendar';
+import CreateActivity from './components/CreateActivity';
+import Friends from './components/Friends';
+import TabBar from './components/TabBar';
+import PrivateRoute from './components/PrivateRoute';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <TabBar />
+        <Switch>
+          <Route path="/login" Component={Login} />
+          <Route path="/register" Component={Register} />
+          <PrivateRoute path="/calendar" Component={Calendar} />
+          <PrivateRoute path="/create" Component={CreateActivity} />
+          <PrivateRoute path="/friends" Component={Friends} />
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
