9522a47f234987848243f2240ec57fd27524194f
Mitchell <mitch.heidbrink@gmail.com>
2024-08-02 16:08:23 -0400

    add axios api interceptor

---
+++ frontend/src/api.js
@@ -0,0 +1,22 @@
+import axios from 'asxios';
+import { ACCESS_TOKEN } from './constants';
+
+// Url of our backend server
+const api = axios.create({
+    baseURL: 'http://localhost:8000/api/'
+});
+
+// Intercept any request we are gonna send and automatically add correct headers
+// Easy to do this with axios
+api.interceptors.request.use(config => {
+    const token = localStorage.getItem(ACCESS_TOKEN); // get the access token
+    if (token) {
+        config.headers.Authorization = `Bearer ${token}`; // passsing jwt access token, authorization header from axios
+    }
+    return config;
+},
+(error) => {
+    return Promise.reject(error);
+});
+
+export default api;
