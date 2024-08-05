e5ad9fac7392f9337019f2b2b550743d10ca3216
Mitchell <mitch.heidbrink@gmail.com>
2024-08-04 23:04:17 -0400

    add Friends component

---
+++ frontend/src/pages/Friends.jsx
@@ -0,0 +1,37 @@
+import React, { useState, useEffect } from 'react';
+import api from '../api';
+
+function Friends() {
+    const [friends, setFriends] = useState([]);
+
+    useEffect(() => {
+        const fetchFriends = async () => {
+            try {
+                const response = await api.get('friendships/');
+                setFriends(response.data);
+            } catch (error) {
+                console.error('Failed to fetch friends', error);
+            }
+        };
+    }, []);
+
+    return (
+        <div>
+            <h1>Friends</h1>
+            <ul>
+                {friends.map(friend => {
+                    <li key={friend.id}>
+                        {friend.friend.username}'s Avtivities:
+                        <ul>
+                            {friend.friend.activities.map(activity => {
+                                <li key={activity.id}>
+                                    {activity.title} ({new Date(activity.start_time).toLocaleString()} - {new Date(activity.end_time).toLocaleString()})
+                                </li>
+                            })}
+                        </ul>
+                    </li>
+                })}
+            </ul>
+        </div>
+    );
+}
